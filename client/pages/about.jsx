080aae541ea4115020d0eb5a9eabc0a1b42b45e0
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-07 17:05:53 +0800

    Initial Commit

---
+++ client/pages/about.jsx
@@ -0,0 +1,17 @@
+import styles from "@/styles/Home.module.css";
+
+export default function About() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          assumenda adipisci animi nulla ab quod vitae eum, voluptas dolore
+          repudiandae molestiae officia sed deserunt necessitatibus magni in
+          doloribus recusandae quaerat?
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ client/pages/contact.jsx
@@ -0,0 +1,17 @@
+import styles from "@/styles/Home.module.css";
+
+export default function Contact() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>Contact Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+          suscipit delectus iure temporibus, a, blanditiis enim aliquam esse
+          consequuntur doloremque, fugiat repellat adipisci quisquam ipsum
+          autem? Reiciendis consequuntur expedita rerum.
+        </p>
+      </main>
+    </>
+  );
+}
