41fc407b7de9935171ca53d4732c377eddf73429
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-08 11:56:18 +0800

    Added new book pages with CRUD (get all, delete, edit, add)

---
+++ client/pages/books/add.jsx
@@ -0,0 +1,55 @@
+import Header from "@/components/header";
+import Head from "next/head";
+import Router from "next/router";
+
+const submitBook = async (event) => {
+  event.preventDefault();
+  const title = event.target.title.value;
+  const url = "http://127.0.0.1:8000/api/books";
+  const res = await fetch(url, {
+    body: JSON.stringify({
+      books: [
+        {
+          title: title,
+        },
+      ],
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+  if (res.status == 201) {
+    // Success
+    Router.push("/books");
+  }
+};
+
+export default function Books() {
+  return (
+    <>
+      <Head>
+        <title>Books | New</title>
+      </Head>
+      <Header>Add New Book</Header>
+      <form className="flex flex-col" onSubmit={submitBook}>
+        <label htmlFor="name" className="mb-2 italic">
+          Name
+        </label>
+        <input
+          className="mb-4 border-b-2"
+          id="title"
+          name="title"
+          type="text"
+          required
+        />
+        <button
+          type="submit"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
